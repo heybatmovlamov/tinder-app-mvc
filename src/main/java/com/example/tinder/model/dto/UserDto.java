0214095c03f1d3f5043cdd1f228164dc68d8e7df
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-07-24 09:41:04 +0400

    UserDto update

--- src/main/java/com/example/tinder/model/dto/UserDto.java
+++ src/main/java/com/example/tinder/model/dto/UserDto.java
@@ -2,14 +2,22 @@ package com.example.tinder.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
-
+    private Long id;
     private String email;
     private String password;
     private LocalDateTime loginTime;
+
+    public UserDto(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
 }
