c29acd7143691eeef2f7b295bae12795561266b0
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-07-24 09:42:51 +0400

    UserRepoImpl update

--- src/main/java/com/example/tinder/model/repository/userRepo/UserRepoImpl.java
+++ src/main/java/com/example/tinder/model/repository/userRepo/UserRepoImpl.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -20,16 +22,35 @@ public class UserRepoImpl implements UserRepository {
     @Override
     public Optional<UserEntity> login(String email, String password) {
         UserEntity userEntity = null;
-        String sql = "SELECT * FROM users WHERE email = ? AND password = ?";
 
-        PreparedStatement preparedStatement = jdbcConfig.getConnection().prepareStatement(sql);
-        preparedStatement.setString(1, email);
-        preparedStatement.setString(2, password);
+        String selectQuery = "SELECT * FROM users WHERE   email = ? AND password = ?";
+        PreparedStatement selectStatement = jdbcConfig.getConnection().prepareStatement(selectQuery);
+        selectStatement.setString(1, email);
+        selectStatement.setString(2, password);
 
-        ResultSet resultSet = preparedStatement.executeQuery();
+        ResultSet resultSet = selectStatement.executeQuery();
+
+        // Update the login_time for the logged in user (assuming 'login_time' is a column in 'users' table)
+        String updateQuery = "UPDATE users SET login_time = ? WHERE email = ?";
+        PreparedStatement updateStatement = jdbcConfig.getConnection().prepareStatement(updateQuery);
+        updateStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+        updateStatement.setString(2, email);
+        updateStatement.executeUpdate();
+
+
+
         if (resultSet.next()) {
-            userEntity = new UserEntity(resultSet.getString("email"), resultSet.getString("password"));
+            userEntity = new UserEntity(
+                    resultSet.getLong("id"),
+                    resultSet.getString("email"),
+                    resultSet.getString("password"),
+                    resultSet.getLong("person_id")
+            );
+
+            updateStatement.close();
         }
+
+        selectStatement.close();
         return Optional.ofNullable(userEntity);
     }
 }
