f39e096dfaab45b3daa49ba7c714e6c77b7e1114
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-08-01 23:29:15 +0400

    ProfileRepository

--- src/main/java/com/example/tinder/model/repository/profileRepo/ProfileRepository.java
+++ src/main/java/com/example/tinder/model/repository/profileRepo/ProfileRepository.java
@@ -1,14 +1,13 @@
-package com.example.tinder.model.repository.PeopleRepo;
+package com.example.tinder.model.repository.profileRepo;
 
 import com.example.tinder.model.entity.ProfileEntity;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ProfileRepository {
-    List<ProfileEntity> users ();
+    List<ProfileEntity> getAllUsers ();
 
 
     void likeOrDislike(boolean like, long userId, long likedUserId);
