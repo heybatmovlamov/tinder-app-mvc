8dd65084b4370d1397904d4daa85056872ff0d3f
heybatmovlamov <heybetmovlamov2005@gmail.com>
2024-08-01 23:28:04 +0400

    MessageRepository

---
+++ src/main/java/com/example/tinder/model/repository/messageRepo/MessageRepository.java
@@ -0,0 +1,12 @@
+package com.example.tinder.model.repository.messageRepo;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository {
+    String sendMessage(String message , long userId);
+    String getMessage(long userId);
+    List<Integer> chooseLikedUser(long id);
+}
